f5947ca80ef344eb6fe2b71f931ebb594b6f4838
Toshiki Nagahama <exevolute@gmail.com>
2021-12-26 18:35:22 +0900

    first commit

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"chat/model"
 	"fmt"
+	"net/http"
 
 	"chat/config"
 
@@ -13,7 +14,10 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-func hello(c echo.Context) error {
+//database
+var db *gorm.DB
+
+func websocketHandler(c echo.Context) error {
 	c.Request().Header.Set( "Access-Control-Allow-Origin", c.Request().RemoteAddr)
 	c.Request().Header.Set( "Access-Control-Allow-Credentials", "true" )
     c.Request().Header.Set( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" )
@@ -40,28 +44,61 @@ func hello(c echo.Context) error {
 	return nil
 }
 
+func getUsers(c echo.Context) error {
+  users := []model.User{}
+  db.Find(&users)
+  return c.JSON(http.StatusOK, users)
+}
+
+func createUser(c echo.Context) error {
+  user := model.User{}
+  if err := c.Bind(&user); err != nil {
+    return err
+  }
+	if user.Name != "" && user.Password != ""{
+		db.Create(&user)
+		return c.JSON(http.StatusCreated, user)
+	}else{
+		map_data := map[string]interface{}{
+        "result": "error 1",
+    }
+		return c.JSON(http.StatusOK, map_data)
+	}
+}
+
+func deleteUser(c echo.Context) error {
+  id := c.Param("id")
+  db.Delete(&model.User{}, id)
+  return c.NoContent(http.StatusNoContent)
+}
+
 func main() {
 	cfg, err := config.Load()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(cfg.Version)
-	db, err := gorm.Open("sqlite3", "chat.sqlite3")
+	db, err = gorm.Open("sqlite3", "chat.sqlite3")
 	if err != nil {
 		panic(err.Error())
 	}
+	defer db.Close()
 
-	db.AutoMigrate(&model.User{})
-	db.Create(&model.User{Name: "toshiki"})
-	var currentUser model.User
-	db.First(&currentUser)
+	//db.AutoMigrate(&model.User{})
+	//db.Create(&model.User{Name: "toshiki", Password: "toshiki"})
+	// var currentUser model.User
+	// db.First(&currentUser)
 
-	fmt.Println(currentUser)
+	// fmt.Println(currentUser)
+
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Static("/", "../frontend")
-	e.GET("/ws", hello)
+	e.GET("/ws", websocketHandler)
+	e.GET("/users", getUsers)
+	e.POST("/create_user", createUser)
+	e.POST("/delte_user", deleteUser)
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- backend/model/model.go
+++ backend/model/model.go
@@ -6,4 +6,5 @@ import "github.com/jinzhu/gorm"
 type User struct {
 	gorm.Model
 	Name string
+	Password string
 }
