faf298e24db7b9b5f27db1bd2755b8f819f86f10
toshikinagahama <exevolute@gmail.com>
2022-01-29 17:14:13 +0900

    環境変数の設定

--- backend/config/config.go
+++ backend/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -21,21 +23,20 @@ type Config struct {
 }
 
 func Load() (*Config, error) {
-	viper.SetConfigName("config")               // 設定ファイル名を指定
-	viper.SetConfigType("yaml")                 // 設定ファイルの形式を指定
-	viper.AddConfigPath("config/environments/") // ファイルのpathを指定
-
-	err := viper.ReadInConfig() // 設定ファイルを探索して読み取る
+	viper.SetConfigName("config")                          // 設定ファイル名を指定
+	viper.SetConfigType("yaml")                            // 設定ファイルの形式を指定
+	viper.AddConfigPath("config/environments/")            // ファイルのpathを指定
+	viper.AutomaticEnv()                                   //環境変数の読み込み
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) //プリフィックスの設定
+	err := viper.ReadInConfig()                            // 設定ファイルを探索して読み取る
 	if err != nil {
 		return nil, fmt.Errorf("failed to load config file- %s", err)
 	}
-
 	var cfg Config
-
 	err = viper.Unmarshal(&cfg)
 	if err != nil {
 		return nil, fmt.Errorf("unmarshal error- %s", err)
 	}
-
+	log.Println(cfg.DBUser)
 	return &cfg, nil
 }
